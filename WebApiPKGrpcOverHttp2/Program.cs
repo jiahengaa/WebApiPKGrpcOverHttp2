0075a7c4e5aad0729d8acb282d1b00965194725a
yf-server\liujiaheng <2429803999@qq.com>
2019-09-19 17:15:39 +0800

    webapi 基于http2协议 与 grpc 基于http2 与webapi 基于http1.1协议 性能对比

---
+++ WebApiPKGrpcOverHttp2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace WebApiPKGrpcOverHttp2 {
+    class Program {
+        static long totalCount = 10000;
+        static async Task Main (string[] args) {
+            while (true) {
+
+                Console.WriteLine ("1 GRPC Test, 2 WebApi2 Test, 3 WebApi1 Test");
+
+                switch (Console.ReadLine ()) {
+                    case "1":
+                        {
+                            var totalTime = await GrpcTest ();
+                            Console.WriteLine ($"GRPC takes:{totalTime}毫秒");
+                        };
+                        break;
+                    case "2":
+                        {
+                            var totalTime = await WebApi2Test ();
+                            Console.WriteLine ($"WebApi2 takes:{totalTime}毫秒");
+                        };
+                        break;
+                    case "3":
+                        {
+                            var totalTime = await WebApi1Test();
+                            Console.WriteLine($"WebApi1 takes:{totalTime}毫秒");
+                        };
+                        break;
+                }
+            }
+        }
+
+        static async Task<long> GrpcTest () {
+            var channel = GrpcChannel.ForAddress ("https://localhost:2000");
+            var client = new Greeter.GreeterClient (channel);
+
+            Stopwatch s = new Stopwatch ();
+            s.Start ();
+
+            for (int i = 0; i < totalCount; i++) {
+                await client.SayHelloAsync (new HelloRequest { Name = "GreeterClient" });
+                //var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+                //Console.WriteLine("Greeting: " + reply.Message);
+            }
+
+            return s.ElapsedMilliseconds;
+        }
+
+        static async Task<long> WebApi2Test () {
+            Stopwatch s = new Stopwatch ();
+            s.Start ();
+            HttpClient client = new HttpClient () {
+                BaseAddress = new Uri ("https://localhost"),
+                DefaultRequestVersion = new Version (2, 0)
+            };
+            for (int i = 0; i < totalCount; i++) {
+                await client.GetStringAsync ("https://localhost/api/values");
+                //Console.WriteLine(await client.GetStringAsync("https://localhost/api/values"));
+            }
+
+            return s.ElapsedMilliseconds;
+        }
+
+        //http://10.1.30.251:1009/AssetCenter/Values/1
+
+        static async Task<long> WebApi1Test()
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://localhost:3000")
+            };
+            for (int i = 0; i < totalCount; i++)
+            {
+                await client.GetStringAsync("https://localhost:3000/api/Values");
+                //Console.WriteLine(await client.GetStringAsync("https://localhost/api/values"));
+            }
+
+            return s.ElapsedMilliseconds;
+        }
+    }
+}
---
+++ WebApiServer/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 80);
+                    options.Listen(IPAddress.Any, 443, listenOptions =>
+                    {
+                        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+                        listenOptions.UseHttps();
+                    });
+                })
+                .UseStartup<Startup>();
+    }
+}
